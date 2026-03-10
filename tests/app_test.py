cdfa40785de183b63c366e8c8b340038882d3817
SRavi98 <jay693381@gmail.com>
2026-03-10 12:24:00 +0530

    added test.py and Ci updated with test

---
+++ tests/app_test.py
@@ -0,0 +1,11 @@
+
+import unittest
+import subprocess
+
+class TestApp(unittest.TestCase):
+	def test_output(self):
+		result = subprocess.run(['python', 'app/app.py'], capture_output=True, text=True)
+		self.assertEqual(result.stdout.strip(), 'Hello, World!')
+
+if __name__ == '__main__':
+	unittest.main()
